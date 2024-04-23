b77f6d646b1e0b3664cb0e9c152c7f0faaaad8dd
Ananda Biswas <anandabiswas751@gmail.com>
2024-04-23 17:45:28 +0600

    commit

--- project/backend/db.js
+++ project/backend/db.js
@@ -3,7 +3,7 @@ const mongoURI = 'mongodb+srv://ananda:1234567890@cluster0.dptnpyz.mongodb.net/p
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to MongoDB Atlas');
   } catch (error) {
     console.error('Error connecting to MongoDB Atlas:', error);
--- project/backend/index.js
+++ project/backend/index.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
+const express = require('express')
+const app = express()
+const port = 5000
 const connectToMongoDB = require('./db');
+
+
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', "frame-ancestors 'self' https://www.youtube.com");
   next();
 });
+
+app.get('/',(req,res)=>
+{
+  res.send("Hello world")
+})
+app.use(express.json())
+app.use('/api',require("./Routes/createuser"));
+
 connectToMongoDB().then(() => {
-  
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
